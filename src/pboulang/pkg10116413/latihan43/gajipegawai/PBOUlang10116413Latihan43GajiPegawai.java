21f72b018a4c7e7ad57b8b80b6e73bef46962297
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-23 15:35:46 +0700

    PBOUlang-10116413-Latihan43-GajiPegawai

---
+++ src/pboulang/pkg10116413/latihan43/gajipegawai/PBOUlang10116413Latihan43GajiPegawai.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan43.gajipegawai;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : Program ini untuk Gaji Pegawai
+ * 
+ */
+public class PBOUlang10116413Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+
+}
